b1be1c9fa8de5b5179d1d9587a947a3f05c084bb
Carson Krueger <crkruege@gmail.com>
2025-03-02 09:43:28 -0700

    add permissions to migration

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS auth.privilege_levels_privileges;
+
+DROP TABLE IF EXISTS auth.privilege_levels;
+
+DROP TABLE IF EXISTS auth.privileges;
+
+DROP TABLE IF EXISTS auth.users;
+
+DROP SCHEMA IF EXISTS auth CASCADE;
